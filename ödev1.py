0e412852c676249d315e8435064dcb22aa6cb1fe
Zakir Mustafa Kemal Kalayci <118366212+ZakirMK@users.noreply.github.com>
2022-11-16 18:30:30 +0300

    ödev

---
+++ ödev1.py
@@ -0,0 +1,21 @@
+baslik = "HABERİNİZ OLSUN" #string
+vade = 12 #integer
+faizOrani1= 1.47 #float
+faizOrani2= 1.44 #float
+
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani1))
+
+mesaj = "Hoşgeldin"
+musteriAdi = "Zakir"
+musteriSoyAdi = "Kalayci"
+sonucMesaj = mesaj + " " + musteriAdi + " " + musteriSoyAdi
+print(sonucMesaj)
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1 + sayi2)
+print(sonucMesaj)
