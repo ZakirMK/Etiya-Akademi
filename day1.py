efab27f155f055ecb80cd7acf4306d5daf13bca9
Zakir Mustafa Kemal Kalayci <118366212+ZakirMK@users.noreply.github.com>
2022-11-17 14:05:56 +0300

    14.00

---
+++ day1.py
@@ -0,0 +1,53 @@
+print("Merhaba Etiya")
+# string = metinsel ifade
+text = "10"
+print(text)
+
+#integet = tam sayı
+number = 10
+print(number)
+
+# double,float,decimal = ondalıklı sayı
+dnumber =5.3
+print(dnumber)
+
+# matematiksel operatörler
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 3) # 10/3 = 1
+
+# boolean,bool = true veya false
+isVerified = False
+
+# mantıksal,karşılaştırma operatörler => her mantıksal operatör boolean değer döner
+print(1 == 2)
+print(2 != 2)
+
+print(2 > 2)
+print(2 < 2)
+
+print(2 >= 2)
+print(2 <= 2)
+
+print(10%5==0)
+
+print(50/2 == 25)
+print(50/2 == 25.0)
+
+#string operasyonları
+text = "Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("Mer"))
+print(text.endswith("Etiya"))
+
+name = "Halit"
+age = "23"
+company = "Kodlamio"
+
+#Halit 23 yaşında Kodlamaio'da çalışıyor.
+print(name + " " + age + " yaşında " + company + "'de çalışıyor.")
+print(f"{name} {age} yaşında {company}'de çalışıyor.") # age = 23 yapsaydıkta bu kod çalışır, bir üst kod çalışmazdı.
+
